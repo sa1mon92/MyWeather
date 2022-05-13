762a0e0baf5cd2e1f1eb2ec0ba0fd42bc0400878
sa1mon92 <79851806+sa1mon92@users.noreply.github.com>
2022-05-13 15:38:07 +0300

    Initial Commit

---
+++ MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by Дмитрий Садырев on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
