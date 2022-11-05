4d353c5d90bc75a9770172801651082de722bda1
Dmitrii Sadyrev <79851806+sa1mon92@users.noreply.github.com>
2022-11-05 13:55:21 +0300

    Update Constats.swift

--- MyWeather/Helpers/Constats.swift
+++ MyWeather/Helpers/Constats.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-public let APIKey = "918575e9973a538206f99ee7305be0e9"
+public let APIKey = "YOUR API KEY OPENWEATHERMAP"
 
 public let startColor = Color(red: 115 / 255, green: 253 / 255, blue: 255 / 255)
 public let endColor = Color(red: 118 / 255, green: 214 / 255, blue: 255 / 255)
